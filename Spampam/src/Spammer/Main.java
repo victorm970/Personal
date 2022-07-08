02d1bdf99291bb96ed75e36167b51a467ea21590
Xeredeth <Xeredeth@DESKTOP-PA5FUCQ>
2022-07-09 01:52:51 +0200

    Spam bot for text!

---
+++ Spampam/src/Spammer/Main.java
@@ -0,0 +1,47 @@
+package Spammer;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) throws AWTException, InterruptedException {
+		
+		int num = 1;
+		Robot robot = new Robot();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter your text:");
+		String Text = scanner.nextLine();
+		
+		StringSelection stringSelection = new StringSelection(Text);
+		Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+		clipboard.setContents(stringSelection, null);
+		
+		System.out.println(" The spamming will start in 5 seconds");
+		Thread.sleep(5000);
+		
+		
+		while(num <= 10) {
+			
+			Thread.sleep(250);
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		
+		num++;
+		
+		}
+	}
+
+}
